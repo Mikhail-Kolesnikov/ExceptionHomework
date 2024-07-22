95b8e487efe6e37b3624685b2e9129cf94b67ad2
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-07-22 09:47:13 +0200

    add File

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculate calculate = new Calculate();
         File file = new File("numbers.txt");
         FileReader fileReader = new FileReader(file);
-        try(BufferedReader bufferedReader = new BufferedReader(fileReader);) {
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader);) {
 
             String line = bufferedReader.readLine();
             int number = Integer.parseInt(line);
@@ -20,16 +20,3 @@ public class Main {
     }
 }
 
-//  File file = new File("numbers.txt");
-//        FileReader fileReader = new FileReader(file);
-//        BufferedReader bufferedReader = new BufferedReader(fileReader);
-//
-//        while (true) {
-//            String line = bufferedReader.readLine();
-//            if (line == null) {
-//                break;
-//            }
-//            int number = Integer.parseInt(line);
-//            int i = 0;
-//            i++;
-//            numb[i] = number;
