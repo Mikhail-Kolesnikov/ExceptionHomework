a24bc914d501ee1a809ace837748f25877334b7b
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-07-22 09:46:06 +0200

    add File

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Calculate calculate = new Calculate();
         File file = new File("numbers.txt");
         FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String line = bufferedReader.readLine();
-        int number = Integer.parseInt(line);
-        Calculate calculate = new Calculate();
-        try {
+        try(BufferedReader bufferedReader = new BufferedReader(fileReader);) {
+
+            String line = bufferedReader.readLine();
+            int number = Integer.parseInt(line);
+
+
             calculate.calculateInside(number);
         } catch (RuntimeException e) {
             e.printStackTrace();
+            System.out.println("Сумма больше 10000");
 
         }
     }
